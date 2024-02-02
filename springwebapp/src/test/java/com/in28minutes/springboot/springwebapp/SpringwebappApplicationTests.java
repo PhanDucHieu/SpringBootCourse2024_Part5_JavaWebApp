641daafcb918c96702fe2c9d8de631f770818ef0
kulutest03 <kulu.test03@kulu.com>
2024-02-02 18:00:37 +0700

    Push project

---
+++ springwebapp/src/test/java/com/in28minutes/springboot/springwebapp/SpringwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.springwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
