5be49873ed6d8af09387ff0ae0a3e36596fc7551
kulutest03 <kulu.test03@kulu.com>
2024-02-07 09:20:19 +0700

    Push project

---
+++ springwebapp/src/main/java/com/in28minutes/springboot/springwebapp/hello/SayHelloController.java
@@ -0,0 +1,40 @@
+package com.in28minutes.springboot.springwebapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+    //"say-hello" = "Hello! What are you learning today?"
+    // http://localhost:8080/
+    @RequestMapping("say-hello")
+    @ResponseBody
+    public String sayHello(){
+        return "Hello! What are you learning today?";
+    }
+
+    @RequestMapping("say-hello-html")
+    @ResponseBody
+    public String sayHelloHtml(){
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("<html>");
+        buffer.append("<head>");
+        buffer.append("<title> My first HTML Page - Changed </title>");
+        buffer.append("</head>");
+        buffer.append("<body>");
+        buffer.append("My first html page with body - Changed");
+        buffer.append("</body>");
+        buffer.append("</html>");
+        return buffer.toString();
+    }
+
+    //sayHello.jsp
+    //"say-hello-jsp" => sayHello.jsp
+    //META-INF/resources/WEB-INF/jsp/sayHello.jsp
+    @RequestMapping("say-hello-jsp")
+    @ResponseBody
+    public String sayHelloJsp(){
+        return "sayHello";
+    }
+}
---
+++ springwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My first html page - jsp</title>
+</head>
+<body>
+    My first html page - jsp
+</body>
+</html>
